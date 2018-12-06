72f1f25a9cd328c6fd46d795ac07a56f6c771031
IrinaVG <irinavit@i.ua>
2018-12-06 14:07:00 +0200

    v1.2 (сформировал список)

--- newpapkakarl/script.py
+++ newpapkakarl/script.py
@@ -11,20 +11,15 @@ with open('orders.csv',encoding='utf-8') as f:
         else:
             d[key[0]]=[key[1:]]
             print(d[key[0]])
-'''for i in d:
-    newk=dict()
-    for f in d[i]:
-        if f[0] in newk:
-            newk[f[0]].append(f[1:])
-            print(newk[f[0]])
-        else:
-            newk[f[0]]=[f[1:]]
-            print(newk[f[0]])
-    print(newk)
-    d[i]=newk
-print(d)'''
+
 def qwe(d):
     for i in d:
+        try:
+            float(d[i][0][0])
+            print(d[i][0][0])
+            return
+        except:
+            pass# как работать с try и except
         newk = dict()
         for f in d[i]:
             if f[0] in newk:
@@ -35,9 +30,12 @@ def qwe(d):
                 #print(newk[f[0]])
         '''print(newk)'''
         d[i] = newk
+        qwe(d[i])
 print(d)
 qwe(d)
 print(d)
 for r in d:
-    qwe(d[r])
+    for i in d[r]:
+        for j in d[r][i]:
+            d[r][i][j]={'price':d[r][i][j][0][1],'qunt':d[r][i][j][0][0]}
 print(d)
